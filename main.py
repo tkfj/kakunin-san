02895099b6050ee24265219b547aaf78b00cdb6f
tkfj <tk@fjworks.jp>
2026-05-31 06:27:33 +0000

    レスポンス情報を追加

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def get_ua(request: Request, response: Response):
     response.headers["Critical-CH"] = requested_hints
     response.headers["Permissions-Policy"] = "ch-ua-full-version-list=(self), ch-ua-arch=(self), ch-ua-model=(self), ch-ua-platform-version=(self)"
 
-    target_headers = [
+    ua_keys = [
         "user-agent",
         "sec-ch-ua",
         "sec-ch-ua-full-version-list",
@@ -23,28 +23,70 @@ async def get_ua(request: Request, response: Response):
         "sec-ch-ua-model",
         "sec-ch-ua-arch",
     ]
+    other_keys = [
+        "accept",
+        "accept-language",
+        "accept-encoding",
+        "referer",
+        "dnt", # Do Not Track
+        "upgrade-insecure-requests",
+        "sec-fetch-site",
+        "sec-fetch-mode",
+        "sec-fetch-dest",
+        "sec-fetch-user",
+    ]
 
     # レスポンス用の辞書を構築
-    ua_info = {}
-    for header in target_headers:
-        value = request.headers.get(header)
-        if value:
-            ua_info[header] = value
-
+    ua_headers = {}
+    other_headers = {}
+    for header in request.headers.keys():
+        h = header.lower()
+        if h in ua_keys:
+            ua_headers[header] = request.headers.get(header)
+        elif h in other_keys:
+            other_headers[header] = request.headers.get(header)
+        
     # クライアントIPの取得
-    x_forwarded_for = request.headers.get("x-forwarded-for")
-    if x_forwarded_for:
-        # Comma-separated list の最初のIPが、真のクライアントIP
-        client_ip = x_forwarded_for.split(",")[0].strip()
+    # x_forwarded_for = request.headers.get("x-forwarded-for")
+    # if x_forwarded_for:
+    #     # Comma-separated list の最初のIPが、真のクライアントIP
+    #     client_ip = x_forwarded_for.split(",")[0].strip()
+    cf_connecting_ip = request.headers.get("cf-connecting-ip")
+    if cf_connecting_ip:
+        client_ip = cf_connecting_ip
     else:
         client_ip = request.client.host
-        
-    return {
-        "status":"OK",
-        "message": "Hi, this is kakunin-san!",
-        "ua_info": ua_info,
-        "client_ip": client_ip
+    detected_country = request.headers.get("cf-ipcountry")
+    detected_city = request.headers.get("cf-city")
+    detected_continent = request.headers.get("cf-ipcontinent")
+    detected_asn = request.headers.get("cf-asn")
+    detected_as_org = request.headers.get("cf-as-organization")
+    network_info = {
+        "ip": client_ip,
     }
+    if detected_country:
+        network_info["country"] = detected_country
+    if detected_city:
+        network_info["city"] = detected_city
+    if detected_continent:
+        network_info["continent"] = detected_continent
+    if detected_asn:
+        network_info["as_number"] = detected_asn
+    if detected_as_org:
+        network_info["as_organization"] = detected_as_org
+
+    ret_values = {
+        "status":"OK",
+        "message": "Hi, this is kakunin-san!"
+    }
+    if ua_headers:
+        ret_values["ua"] = ua_headers
+    if network_info:
+        ret_values["network_info"] = network_info
+    if other_headers:
+        ret_values["headers"] = other_headers
+    return JSONResponse(content=ret_values)
+
 
 @app.api_route("/", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"])
 async def catch_all(request: Request):
